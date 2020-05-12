d178e401c3223caccf49e3412ad5d659e1cd5144
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-12 17:57:59 +0300

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,108 @@
+/* jshint node: true */
+
+module.exports = function (environment) {
+    var ENV = {
+        modulePrefix: 'open-ethereum-pool',
+        environment: environment,
+        rootURL: '/',
+        locationType: 'hash',
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
+            }
+        },
+
+        APP: {
+            // API host and port
+            ApiUrl: '//192.168.0.200/',
+
+            // HTTP mining endpoint
+            HttpHost: 'http://192.168.0.200',
+            HttpPort: 8888,
+
+            // Stratum mining endpoint
+            StratumHost: '192.168.0.200',
+            StratumPort: 8008,
+
+            NicehashHost: '192.168.0.200',
+            NicehashPost: 8089,
+
+            // Fee and payout details
+            PoolFee: '0.99%',
+            PayoutThreshold: '0.01',
+            ShareDifficulty: '4000000000',
+
+            //Current and Localization
+            Currency: 'USD',
+            CoinName: 'EthereumX',
+            CoinShortName: 'ETX',
+            PaymentText: 'every 2 hours',
+            SupportMail: 'helpdesk@daggerpool.com',
+            SupportHelpdesk: 'http://helpdesk.daggerpool.com',
+            WebsiteName: '192.168.0.200',
+
+            //Coin Bases Settings
+            ChainAddress : 'http://www.ethereumx.xyz/address.html',
+	    TransactionAddress : 'http://www.ethereumx.xyz/transations.html',
+            UncleAddress : 'http://www.ethereumx.xyz',
+            BlockAddress : 'http://www.ethereumx.xyz/blocklist.html',
+
+            //Twitter Parameter
+            TwitterURL: 'https://twitter.com/',
+            TwitterHash: 'daggerpool_com',
+
+
+            // For network hashrate (change for your favourite fork)
+            BlockTime: 15
+
+
+        }
+    };
+
+    if (environment === 'development') {
+        /* Override ApiUrl just for development, while you are customizing
+         frontend markup and css theme on your workstation.
+         */
+        ENV.APP.ApiUrl = 'https://eth.daggerpool.com/'
+        // ENV.APP.LOG_RESOLVER = true;
+        // ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    }
+
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.locationType = 'none';
+
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+        ENV.APP.rootElement = '#ember-testing';
+    }
+
+    if (environment === 'production') {
+        ENV.baseURL = '/ember-cli-twitter-feed/';
+    }
+
+    ENV.contentSecurityPolicy = {
+        'script-src': [
+            "'self'",
+            'https://syndication.twitter.com',
+			            'https://cdn.syndication.twimg.com',
+            "'sha256-XnNQECY9o-nIv2Qgcd1A39YarwxTm10rhdzegH_JBxY='"],
+        'style-src': [
+            "'self'",
+            'http://platform.twitter.com',
+            "'sha256-zCvYlDs6LsUp0EqrJFjIGUiM_AG2fGlkNrzJ2YiBTG0='"],
+        'img-src': [
+            "'self'",
+            'data:',
+            'https://pbs.twimg.com',
+            'https://syndication.twitter.com',
+            'http://platform.twitter.com']
+    };
+    return ENV;
+};
