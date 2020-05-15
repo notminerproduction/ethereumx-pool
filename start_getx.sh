48eef844f205341c8096e7e7be537cb29cacf476
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-15 09:03:07 +0300

    Update start_getx.sh

--- start_getx.sh
+++ start_getx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WALLET="0x9dbe1caae874baae91022d170fba246100c73286"
-screen -S getx ./getx --rpc --maxpeers 75 --syncmode "fast" --rpcapi "net,web3,personal" --etherbase "$WALLET" --cache=12288 --mine --unlock "$WALLET" --allow-insecure-unlock --password ~/pwd &
+screen -S getx ./getx --rpc --maxpeers 75 --syncmode "fast" --rpcapi "eth,net,web3,personal" --etherbase "$WALLET" --cache=12288 --mine --unlock "$WALLET" --allow-insecure-unlock --password ~/pwd &
