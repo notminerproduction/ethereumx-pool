a783b30b93698678a38213428b7a1326f7a357c2
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-17 21:30:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,8 +61,8 @@ make
 #sudo rm -rf ~/.npm
 
 cd www/
-#sudo rm package.json
-#mv ~/ethereumx-pool/package.json ~/ethereumx-pool/www
+sudo rm package.json
+mv ~/ethereumx-pool/package.json ~/ethereumx-pool/www
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.bashrc
